292ab0b5e4096a8de08da573ee32f8b6666b7329
Alan Zhu <alanzhu2002@gmail.com>
2020-01-09 17:00:58 -0500

    Added color sensor
    Added color sensor and placeholder values for RobotMap

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,6 +7,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.I2C;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants.  This class should not be used for any other purpose.  All constants should be
@@ -17,30 +19,33 @@ package frc.robot;
  */
 public final class Constants {
     //Climber motors
-    public static final int CLIMB_L;
-    public static final int CLIMB_R;
+    public static final int CLIMB_L = 0;
+    public static final int CLIMB_R = 1;
 
     //Drivetrain motors
-    public static final int DT_R1;
-    public static final int DT_R2;
-    public static final int DT_R3;
-    public static final int DT_L1;
-    public static final int DT_L2;
-    public static final int DT_L3;
+    public static final int DT_R1 = 2;
+    public static final int DT_R2 = 3;
+    public static final int DT_R3 = 4;
+    public static final int DT_L1 = 5;
+    public static final int DT_L2 = 6;
+    public static final int DT_L3 = 7;
 
     //Intake motors
-    public static final int INTAKE;
-    public static final int DELIVERY;
+    public static final int INTAKE = 8;
+    public static final int DELIVERY = 9;
 
     //Intake sensors
-    public static final int DELIVERY_S1;
-    public static final int DELIVERY_S2;
+    public static final int DELIVERY_S1 = 0;
+    public static final int DELIVERY_S2 = 1;
 
     //Shooter motors
-    public static final int SHOOTER_1;
-    public static final int SHOOTER_2;
+    public static final int SHOOTER_1 = 10;
+    public static final int SHOOTER_2 = 11;
 
     //Spinner motors
-    public static final int SPIN;
+    public static final int SPIN = 12;
+
+    //Spinner color sensor
+    public static final I2C.Port I2C_PORT = I2C.Port.kOnboard;
 
 }
--- src/main/java/frc/robot/subsystems/Spinner.java
+++ src/main/java/frc/robot/subsystems/Spinner.java
@@ -11,14 +11,18 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 import com.revrobotics.*;
+import com.revrobotics.ColorSensorV3;
 import com.ctre.phoenix.motorcontrol.can.*;
 
 public class Spinner extends SubsystemBase {
+  private final WPI_TalonSRX spinner = new WPI_TalonSRX(Constants.SPIN);
+  private final ColorSensorV3 colorSensor = new ColorSensorV3(Constants.I2C_PORT);
+
   /**
    * Creates a new Spinner.
    */
   public Spinner() {
-    private final WPI_TalonSRX spinner = new WPI_TalonSRX(Constants.SPIN);
+    
   }
 
   @Override
