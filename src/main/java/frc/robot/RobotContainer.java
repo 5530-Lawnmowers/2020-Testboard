fb33b67b5522750f69894b4618202cdbf82223d9
Amon Green <drwhorx@gmail.com>
2020-01-18 10:23:16 -0500

    Setup Turret subsystem and command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -31,12 +31,14 @@ public class RobotContainer {
   private final Shooter shooter = new Shooter();
   private final Spinner spinner = new Spinner();
   private final Delivery delivery = new Delivery();
+  private final Turret turret = new Turret();
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
   public static XboxController XBController = new XboxController(1);
   public static JoystickButton xba = new JoystickButton(XBController, 1);
   public static JoystickButton xbb = new JoystickButton(XBController, 2);
+  public static JoystickButton xblb = new JoystickButton(XBController, 5);
   public static JoystickButton xbrb = new JoystickButton(XBController, 6);
   public static JoystickButton xby  = new JoystickButton(XBController, 4);
   public static JoystickButton xbx = new JoystickButton(XBController, 3);
@@ -65,6 +67,8 @@ public class RobotContainer {
     xbx.toggleWhenPressed(new SpinnerMotorTest(spinner));
     xbstart.toggleWhenPressed(new DrivetrainMotorTest(drivetrain));
     xbback.toggleWhenPressed(new DeliveryMotorTest(delivery));
+    xblb.toggleWhenPressed(new TurretMotorTest(turret, false));
+    xbrb.toggleWhenPressed(new TurretMotorTest(turret, true));
   }
 
 
