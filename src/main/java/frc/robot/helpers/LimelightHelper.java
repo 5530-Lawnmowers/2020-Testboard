a131b306c81951ad741a775557d01e2a5191434f
Unknown <rkersten@students.greenhillsschool.org>
2020-01-24 19:22:08 -0500

    Add the ability to better interface with limelight though helper

--- src/main/java/frc/robot/helpers/LimelightHelper.java
+++ src/main/java/frc/robot/helpers/LimelightHelper.java
@@ -13,12 +13,24 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 /**
  * Add your docs here.
  */
-public class limelightHelper {
+public class LimelightHelper {
     public void getRawY(){
         NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
         NetworkTableEntry ty = table.getEntry("ty");
         double y = ty.getDouble(0.0);
         System.out.println(y);
+      }public class limelightHelper {
+lic void getRawY()
+        NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+        NetworkTableEntry t = table.getEntry("y");
+        double  = t.getDouble(0.0);
+        System.out.println();
+
+      public static void getRawX(){
+        NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+        NetworkTableEntry tx = table.getEntry("xy");
+        double x = tx.getDouble(0.0);
+        System.out.println(x);
       }
       public static double getDistance() {
         NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
@@ -33,9 +45,3 @@ public class limelightHelper {
 
     public static double getTargetX(double angle) {
         return getDistance() * Math.cos(angle);
-       }
-       public static double getTargetY(double angle){
-        return getDistance() * Math.sin(angle);
-    }
-
-}
