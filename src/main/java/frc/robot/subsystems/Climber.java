7c5fcd6844dfcc1aa5d7dc16a1c0adaec5851c0c
Ryan K <rkersten@greenhillsschool.org>
2020-02-05 15:27:24 -0500

    Adds Base Code For Climb Serrvo

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -9,11 +9,12 @@ package frc.robot.subsystems;
 
 import frc.robot.Constants;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
+import edu.wpi.first.wpilibj.Servo; 
 import com.revrobotics.*;
 import com.ctre.phoenix.motorcontrol.can.*;
 
 public class Climber extends SubsystemBase {
+    private final Servo pinservo = new Servo(8);
     private final WPI_TalonSRX climbLeft = new WPI_TalonSRX(Constants.CLIMB_L);
     private final WPI_TalonSRX climbRight = new WPI_TalonSRX(Constants.CLIMB_R);
 
@@ -26,15 +27,25 @@ public class Climber extends SubsystemBase {
 
     @Override
     public void periodic() {
+       
+
         // This method will be called once per scheduler run
     }
 
     public void testClimbSet(double speed) {
         climbLeft.set(speed);
         climbRight.set(speed);
+        pinservo.set(1);
+        System.out.println("Test");
     }
+    public void realeaseArm(){
+        
+    }
+    
 
     public void testClimbStop() {
+        pinservo.set(0);
+
         climbLeft.stopMotor();
         climbRight.stopMotor();
     }
